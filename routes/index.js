d70c1ec7aeb0167206f7425c318239b5a48ec71b
mahesh-sontakke <mahesh-sontakke@users.noreply.github.com>
2018-11-27 15:32:42 +0530

    update middleware cors and bodyparser json

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,15 @@ module.exports = {
         app.get('/*', (req, res) => res.sendFile(__baseUrl + '/client/'));
     },
     bindMiddleware: function (app) {
+        app.use(function (req, res, next) {
+           res.header("Access-Control-Allow-Origin", "*");
+           res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+           next();
+       });
         var bodyParser = require('body-parser');
         var session = require('express-session');
         app.use(session({ secret: CREDENTIALS.SESSION_SECRET_KEY, resave: false, saveUninitialized: true }));
-        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
     },
     bindAuth: function (app) {
         const auth = require('../auth');
