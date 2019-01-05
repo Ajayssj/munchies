3ed87ae49392b4639e85c577dc99c8cc4dc75b9f
mahesh-sontakke <mahesh-sontakke@users.noreply.github.com>
2019-01-05 12:02:58 +0530

    Order summary calculation

--- frontEnd/src/app/user/order-summary/order-summary.component.ts
+++ frontEnd/src/app/user/order-summary/order-summary.component.ts
@@ -23,6 +23,8 @@ export class OrderSummaryComponent implements OnInit {
   coupanCodeError = '';
   extraInfo = [];
   coupanCodeSuccess = '';
+  greenTea = '';
+  weeks: number;
   constructor(
     private http: HttpClient, 
     private router: Router,
@@ -35,11 +37,28 @@ export class OrderSummaryComponent implements OnInit {
       this.router.navigate(['/subscribe']);
     }
     else {
-        console.log(this.customerData)
         this.selectedPlanId = this.customerData.planId;
         this.planTitle = this.customerData.planName;
-        this.subTotal = this.customerData.planRate;
+        console.log(this.planTitle);
+        if(this.planTitle == 'Trial Week') {
+          this.weeks = 1;
+        }
+        else if(this.planTitle == 'For 4 Weeks') {
+          this.weeks = 4;
+        }
+        else {
+          this.weeks = 12;
+        }
+        console.log(this.weeks)
+        this.subTotal = (JSON.parse(this.customerData.planRate) + (50 * this.weeks));
+        console.log(this.subTotal);
         this.extraInfo = JSON.parse(this.customerData.extraInfo);
+        console.log(this.extraInfo);
+        this.extraInfo.forEach(info=> {
+          if(info.type == "green_tea") {
+            this.greenTea = info.value;
+          }
+        });
         // if(extraInfo && extraInfo[0]) {
         //   this.question1 = extraInfo[0].value;
         // }
@@ -87,14 +106,20 @@ export class OrderSummaryComponent implements OnInit {
         else {
           this.discount = (this.subTotal * res.data.discount) / 100;
         }
-        this.coupanCodeSuccess = "Coupan code applied successfully";
+        this.coupanCodeSuccess = 'Coupan code applied successfully';
       }
       else {
+        this.coupanCodeSuccess = '';
         this.coupanCodeError = res.error;
-        console.log(this.coupanCodeError)
-        this.subTotal = this.subTotal;
+        console.log(this.coupanCodeError);
+        if(this.greenTea == "Yes") {
+          this.subTotal = (this.subTotal + (39 * this.weeks));
+        }
+        else {
+          this.subTotal = this.subTotal;
+        }
+        console.log(this.subTotal);
       }
-      console.log(this.subTotal + 50)
     },
     err => {
 
